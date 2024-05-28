9a62e878d82d885f27fb9b5b232707f1eaacaadf
pasha <pasha2003228@gmail.com>
2024-05-29 00:06:53 +0300

    feat: add react-query and some alias

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,52 +1,3 @@
-// module.exports = {
-//   root: true,
-//   env: { browser: true, es2020: true },
-//   extends: [
-//     'eslint:recommended',
-//     'airbnb-typescript',
-//     'airbnb/hooks',
-//     'plugin:@typescript-eslint/recommended',
-//     'plugin:react/recommended',
-//     'plugin:react-hooks/recommended',
-//     'plugin:jsx-a11y/recommended',
-//     'plugin:import/errors',
-//     'plugin:import/warnings',
-//     'plugin:import/typescript',
-//     'plugin:prettier/recommended',
-//     'prettier/@typescript-eslint',
-//     'prettier/react',
-//     'prettier',
-//   ],
-//   ignorePatterns: ['dist', '.eslintrc.cjs'],
-//   parser: '@typescript-eslint/parser',
-//   plugins: [
-//     '@typescript-eslint',
-//     'react',
-//     'react-hooks',
-//     'jsx-a11y',
-//     'import',
-//     'simple-import-sort',
-//     'prettier'
-//     ,'react-refresh'
-//   ],
-//   rules: {
-//     'prettier/prettier': 'error',
-//     'import/order': 'off',
-//     'simple-import-sort/imports': 'error',
-//     'simple-import-sort/exports': 'error',
-//     'react/react-in-jsx-scope': 'off',
-//     'react/jsx-filename-extension': [
-//       1,
-//       { extensions: ['.jsx', '.tsx'] },
-//     ],
-//     'react/prop-types': 'off',
-//     'react-refresh/only-export-components': [
-//       'warn',
-//       { allowConstantExport: true },
-//     ],
-//   },
-// }
-
 module.exports = {
   parser: '@typescript-eslint/parser',
   ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
@@ -95,6 +46,9 @@ module.exports = {
   settings: {
     react: {
       version: 'detect',
+    },
+    "import/resolver": {
+      "typescript": {}
     },
   },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import react from '@vitejs/plugin-react';
+
 import { defineConfig } from 'vite';
+import * as path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,4 +11,14 @@ export default defineConfig({
     strictPort: true,
   },
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve('./src'),
+      Pages: path.resolve('./src/pages'),
+      Modules: path.resolve('./src/modules'),
+      Components: path.resolve('./src/components'),
+      Ui: path.resolve('./src/ui'),
+      Configs: path.resolve('./src/configs'),
+    },
+  },
 });
